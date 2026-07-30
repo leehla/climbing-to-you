5ad18d7c25d88d36c1119615804cda312d7fa99f
leehla <coelloe.lilibeth@gmail.com>
2026-07-29 21:29:58 -0500

    Update config.js

--- site/config.js
+++ site/config.js
@@ -6,7 +6,7 @@
  * is enabled. NEVER place a service_role key in this file.
  */
 window.CLIMBING_CONFIG = {
-  supabaseUrl: ""https://xxxxx.supabase.co",
+  supabaseUrl: "https://xxxxx.supabase.co",
   supabaseAnonKey: "sb_publishable_xxxxxxxxx",
   maxRecordingSeconds: 180,
 };
